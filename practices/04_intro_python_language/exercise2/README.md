cedcc52e0fe7f221ba31877215ca9c29f4ab566b
Erick Obregón Fonseca <erickobregonf@gmail.com>
2023-12-31 00:52:13 -0600

    [Add] Dictionary from Spanish to English

---
+++ practices/04_intro_python_language/exercise2/README.md
@@ -0,0 +1,2 @@
+# **Exercise 2**
+Create a dictionary type variable, which contains at least 20 English words and has its Spanish translation as a key.
---
+++ practices/04_intro_python_language/exercise2/dict_es_en.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Erick Andres Obregon Fonseca
+Copyright: Copyright 2023, CE1101-Introduction-to-Programming
+Credits: Erick Andres Obregon Fonseca
+License: MIT
+Version: 1.0.0
+Maintainer: Erick Andres Obregon Fonseca
+Email: erickobregonf@gmail.com,erickof@ieee.org,erickof@estudiantec.cr
+Status: Development
+
+Description:
+    Create a dictionary type variable, which contains at least 20 English words and has its
+    Spanish translation as a key.
+
+Usage:
+    $python exercise1.py
+"""
+
+from typing import *
+
+__author__ = "Erick Andres Obregon Fonseca"
+__copyright__ = "Copyright 2023, CE1101-Introduction-to-Programming"
+__credits__ = ["Erick Andres Obregon Fonseca"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Erick Andres Obregon Fonseca"
+__email__ = "erickobregonf@gmail.com,erickof@ieee.org,erickof@estudiantec.cr"
+__status__ = "Development"
+
+dict_es_en: Dict[str, str] = {
+    'Adios': 'Bye',
+    'Amigo': 'Friend',
+    'Ciudad': 'City',
+    'Comida': 'Food',
+    'Dia': 'Day',
+    'Enemigo': 'Enemy',
+    'Hermana': 'Sister',
+    'Hermano': 'Brother',
+    'Hola': 'Hello',
+    'Humano': 'Human',
+    'Jefe': 'Boss',
+    'Libro': 'Book',
+    'Manzana': 'Apple',
+    'Monstruo': 'Monster',
+    'Naranja': 'Orange',
+    'Noche': 'Night',
+    'Pajaro': 'Bird',
+    'Pelicula': 'Movie',
+    'Tiempo': 'Time',
+    'Trabajo': 'Job'
+}
